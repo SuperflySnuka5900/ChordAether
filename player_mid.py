876cc4e00b0f953ee047aa7ed9f70536c436ee2b
graceyu12 <gyu12@terpmail.umd.edu>
2025-04-20 21:18:32 -0400

    Created doc strings to explain functions created

--- player_mid.py
+++ player_mid.py
@@ -8,19 +8,25 @@ import os
 class MidiPlayer:
     """Plays MIDI chords using FluidSynth for reliable cross-platform sound."""
     def __init__(self, soundfont_path="SM64SF V2.sf2"):
-        """"""
+        """Initializes the midi player with a sound font and connects it to the driver"""
         if not os.path.exists(soundfont_path):
             raise FileNotFoundError(f"SoundFont not found: {soundfont_path}")
-        
+        """Raises error if file is not found"""
         self.fs = fluidsynth.Synth()
+        """Creates a synth object in fluidsynth that generates a synthesizer object to control sound generation"""
         self.fs.start(driver=self.get_driver())  # macOS; use "alsa" on Linux or "dsound" on Windows
+        """Refers to the get_driver function to connect the synthesizer to the driver"""
         self.sfid = self.fs.sfload(soundfont_path)
+        """Loads the sound font into the synthesizer"""
         self.fs.program_select(0, self.sfid, 0, 0)
+        """Checks for channel, sound font id, bank, and the preset"""
 
     def set_instrument(self, program, bank=0):
+        """Changes sound bank and program to match different instruments"""
         self.fs.program_select(0, self.sfid, bank, program)
 
     def get_driver(self):
+        """Check for machine's os name and returns name of driver to make program compatible"""
         if os.name == "nt":
             driver = "dsound"
         elif os.name == "posix":
