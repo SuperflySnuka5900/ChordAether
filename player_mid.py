2e2228a90ee67941ba02ace72459133d1194fd17
John <100503544+johnnvalencia@users.noreply.github.com>
2025-04-20 21:51:31 -0400

    Update player_mid.py
    Added more docstrings to each function

--- player_mid.py
+++ player_mid.py
@@ -18,9 +18,23 @@ class MidiPlayer:
         self.fs.program_select(0, self.sfid, 0, 0)
 
     def set_instrument(self, program, bank=0):
+        """ Here we are setting the instrument for the synthesizer 
+
+        Arguments: program which is the MIDI program and bank which is
+        the bank number to select from 
+
+        """
+
         self.fs.program_select(0, self.sfid, bank, program)
 
     def get_driver(self):
+        """ Here we are determining the audio driver
+
+        Returns: the name of the audio driver for either Windows and 
+        macOS/Linux 
+
+        """
+
         if os.name == "nt":
             driver = "dsound"
         elif os.name == "posix":
@@ -28,6 +42,13 @@ class MidiPlayer:
         return driver
     
     def play_chords(self, chord_sequence, tempo=120):
+        """ This is playing a senquence of chords
+
+        Arguments: chord_sequence which is a list and tempo which
+        is the tempo in beats per minute
+
+        """
+
         delay = 60 / tempo
         for chord in chord_sequence:
             for note in chord:
@@ -37,4 +58,8 @@ class MidiPlayer:
                 self.fs.noteoff(0, note)
 
     def close(self):
+        """ Here we shut down and delete the synthesizer instance 
+
+        """
+
         self.fs.delete()
