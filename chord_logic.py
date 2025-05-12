acc3f5b4b6d2ae9fb3159e1b6b2e894bccf8e961
Trent Bowman <trmibo_13579@icloud.com>
2025-05-11 23:55:50 -0400

    Continued to update chord_logic

--- chord_logic.py
+++ chord_logic.py
@@ -9,37 +9,32 @@ import random
 
 class Sequence():
 
-    def __init__(self, key, scale):
+    def __init__(self, key, scale, seq_length, extended = False):
         self.key = key
         self.scale = scale
-        self.indexes = []
-        self.numerals = []
-        self.pitches = []
+        self.generate_sequence(seq_length, extended)
 
 
-    def generate_sequence(self, seq_length, seq_key = -1, seq_scale = "", extended = False):
+    def generate_sequence(self, seq_length, extended = False):
         """Generates a sequence using the given parameters.
         Args:
             seq_length(int): length of the sequence
-            key(int): key being used. -1 defaults to self.key
-            scale(str): scale being used. "" defaults to self.scale
             extended(bool): whether seventh chords will be used. Defaults to false.
-        Returns:
-            list(tuple(int)): the final sequence
+        Side Effects:
+            Creates indexes, numerals, and pitches attributes and populates the lists.
         """
         # All input into this function should be done by the program and not the user.
         # Sequence length must an integer be greater than 1.
-        # If not using default, sequence key must be an integer 0-11.
-        # If not using default, sequence scale must be one of the ones defined in database.
+        
         # set up
-        if seq_key == -1:
-            seq_key = self.key
-        if seq_scale == "":
-            seq_scale = self.scale
+        seq_key = self.key
+        seq_scale = self.scale
         
         diatonic_chords = db.diatonic_chords[seq_scale]
         diatonic_7th_chords = db.diatonic_7th_chords[seq_scale]
-        final_sequence = []
+        
+        self.numerals = []
+        self.pitches = []
 
         # generate seq_length number of chords
         for x in range(seq_length):
@@ -60,13 +55,20 @@ class Sequence():
             else:
                 transposed_chord = [(x + seq_key)%12 for x in diatonic_chords[numeral]]
 
-            # append to our final sequence
-            final_sequence.append(transposed_chord)
+            # append to our sequence attributes
+            self.pitches.append(transposed_chord)
+
+            if len(transposed_chord) == 3: # triad
+                self.numerals.append(db.diatonic_triad_numerals[seq_scale][numeral])
+            else:
+                self.numerals.append(db.diatonic_7th_numerals[seq_scale][numeral])
         
         # after the sequence is generated, make sure that there is at least one I or i chord.
-        tonic = [(x + seq_key)%12 for x in (0, 4, 7)]
-        tonic_7th = [(x + seq_key)%12 for x in (0, 4, 7)]
-        if (tonic not in final_sequence) and (tonic_7th not in final_sequence):
+        tonic = db.diatonic_triad_numerals[seq_scale][0]
+        tonic_7th = db.diatonic_7th_numerals[seq_scale][0]
+
+        if (tonic not in self.numerals) and (tonic_7th not in self.numerals):
+            # determine chord notes
             if extended:
                 # randomly choose if we will add the seventh to the chord
                 seventh = random.choice([True,False])
@@ -77,29 +79,43 @@ class Sequence():
             # otherwise just use triads
             else:
                 transposed_chord = [(x + seq_key)%12 for x in (0, 4, 7)]
-    
-        return final_sequence
+            
+            # edit the existing sequence
+            self.pitches[0] = transposed_chord
+
+            if len(transposed_chord) == 3: # triad
+                self.numerals[0] = db.diatonic_triad_numerals[seq_scale][0]
+            else:
+                self.numerals[0] = db.diatonic_7th_numerals[seq_scale][0]
+        
+    def get_label(self):
+        final_str = self.numerals[0]
+        for i in range(1, len(self.numerals)):
+            final_str += f" – {self.numerals[i]}"
+        return final_str
 
 
 # this is for testing
 if __name__ == "__main__":
 
+    mySeq = Sequence(0,'major')
+
     # TEST 1: Sequence length
     # Each of these should print sequences with 4, 2, and 7 chords, respectively.
     # All of them will be chords from C major and not include extensions.
     print('Test 1')
-    print(generate_sequence(4, 0, 'major'))
-    print(generate_sequence(2, 0, 'major'))
-    print(generate_sequence(7, 0, 'major'))
+    print(mySeq.generate_sequence(4, 0, 'major'))
+    print(mySeq.generate_sequence(2, 0, 'major'))
+    print(mySeq.generate_sequence(7, 0, 'major'))
 
     # TEST 2: Sequence key
     # Each of these should print chords from C, E, and F# major, respectively.
     # Every integer should be between 0 and 11.
     # All of them will include 4 chords and not include extensions.
     print('Test 2')
-    print(generate_sequence(4, 0, 'major'))
-    print(generate_sequence(4, 4, 'major'))
-    print(generate_sequence(4, 6, 'major'))
+    print(mySeq.generate_sequence(4, 0, 'major'))
+    print(mySeq.generate_sequence(4, 4, 'major'))
+    print(mySeq.generate_sequence(4, 6, 'major'))
 
     # TEST 3: Sequence scale
     # Each of these should print chords from C minor, harmonic minor, and mixolydian, respectively.
